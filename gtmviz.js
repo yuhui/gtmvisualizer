07b0bc57125ac7a6c60b638c05795d09bca8d79f
Yuhui <yuhuibc@gmail.com>
2015-07-05 23:43:01 +0800

    First pages commit

---
+++ gtmviz.js
@@ -0,0 +1,142 @@
+/**
+ * Master GTM Visualisation class.
+ */
+var GtmViz = GtmViz || function () {
+  "use strict";
+};
+
+GtmViz.container = null;
+
+/**
+ * @constructor
+ */
+GtmViz.setContainer = function(jsonContainer) {
+  this.container = new GtmLib(jsonContainer);
+};
+
+/**
+ * Builds the list from the given objects.
+ *
+ * @param {string} collection 'tags', 'triggers' or 'variables'
+ * @param {jQuery array} objects tags, triggers or variables
+ */
+GtmViz._buildList = function(collection, objects, log) {
+
+  /**
+   * Builds the list of children from the given object.
+   *
+   * @param {string} childCollection 'tags', 'triggers' or 'variables'
+   * @param {jQuery array} childObjects tags, triggers or variables
+   * @param {jQuery node} parentList The parent list ID
+   */
+  function buildChildren(childCollection, childObjects, parentList) {
+    var childItemName = childCollection.substring(0, 1).toUpperCase() +
+        childCollection.substring(1);
+    var childItem = $('<li>', {
+      text: childItemName
+    });
+    parentList.append(childItem);
+
+    var parentId = parentList.attr('id').replace('_list', '');
+    var childId = parentId + '_' + childCollection;
+    var childList = $('<ul>', {
+      id: childId
+    });
+    childItem.append(childList);
+
+    var childObject, childListItem, childListItemName;
+    for (var x = 0, y = childObjects.length; x < y; x++) {
+      childObject = childObjects[x];
+      childListItem = $('<li>', {
+        id: childId + '_' + childObject.objectId
+      });
+      childList.append(childListItem);
+
+      childListItemName = $('<span>', {
+        class: 'type_' +
+            childCollection.substring(0, childCollection.length - 1),
+        text: childObject.name
+      });
+      childListItem.append(childListItemName);
+    }
+  }
+
+  if (objects.length > 0) {
+    var parentNode = $('#' + collection);
+    parentNode.empty();
+
+    var collectionList = $('<ul>', {
+      id: collection + '_list'
+    });
+    parentNode.append(collectionList);
+
+    for (var i = 0, j = objects.length; i < j; i++) {
+      var object = objects[i];
+
+      var collectionItemId = collection + '_' + object.objectId;
+      var collectionItem = $('<li>', {
+        id: collectionItemId
+      });
+      collectionList.append(collectionItem);
+
+      var collectionItemName = $('<span>', {
+        class: 'type_' + collection.substring(0, collection.length - 1),
+        text: object.name
+      });
+      collectionItem.append(collectionItemName);
+
+      var itemChildrenList = $('<ul>', {
+        id: collectionItemId + '_list',
+        class: 'child_list'
+      });
+      collectionItem.append(itemChildrenList);
+
+      if (object.hasTags && object.hasTags()) {
+        buildChildren('tags', object.getTags(), itemChildrenList);
+      }
+      if (object.hasTriggers && object.hasTriggers()) {
+        buildChildren('triggers', object.getTriggers(), itemChildrenList);
+      }
+      if (object.hasVariables && object.hasVariables()) {
+        buildChildren('variables', object.getVariables(), itemChildrenList);
+      }      
+    }
+  }
+};
+
+/**
+ * Builds the list of objects for the given collection.
+ *
+ * @param {string} collection 'tags', 'triggers' or 'variables'
+ */
+GtmViz._listCollection = function(collection) {
+  if (this.container) {
+    var objects;
+
+    switch (collection) {
+    case 'tags':
+      objects = this.container.getTags();
+      break;
+    case 'triggers':
+      objects = this.container.getTriggers();
+      break;
+    case 'variables':
+      objects = this.container.getVariables();
+      break;
+    }
+
+    this._buildList(collection, objects);
+  }
+};
+
+GtmViz.listTags = function() {
+  this._listCollection('tags');
+};
+
+GtmViz.listTriggers = function() {
+  this._listCollection('triggers');
+};
+
+GtmViz.listVariables = function() {
+  this._listCollection('variables');
+};
---
+++ polyfill.js
@@ -0,0 +1,188 @@
+/**
+ * Polyfills
+ * Taken from Mozilla Developer Network https://developer.mozilla.org.
+ */
+
+if (!String.isString) {
+  String.isString = function(arg) {
+    'use strict';
+    return Object.prototype.toString.call(arg) === '[object String]';
+  };
+}
+
+if (!Array.isArray) {
+  Array.isArray = function(arg) {
+    'use strict';
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
+}
+
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun/*, thisArg*/) {
+    'use strict';
+    if (this === void 0 || this === null) throw new TypeError();
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function') throw new TypeError();
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i];
+        if (fun.call(thisArg, val, i, t)) res.push(val);
+      }
+    }
+    return res;
+  };
+}
+
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) return value;
+    }
+    return undefined;
+  };
+}
+
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function(callback, thisArg) {
+    'use strict';
+    var T, k;
+    if (this == null) throw new TypeError(' this is null or not defined');
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + ' is not a function');
+    }
+    if (arguments.length > 1) T = thisArg;
+    k = 0;
+    while (k < len) {
+      var kValue;
+      if (k in O) {
+        kValue = O[k];
+        callback.call(T, kValue, k, O);
+      }
+      k++;
+    }
+  };
+}
+
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length) || 0;
+    if (len === 0) return false;
+    var n = parseInt(arguments[1]) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) k = 0;
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+          (searchElement !== searchElement &&
+            currentElement !== currentElement)) {
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
+
+if (!Array.prototype.map) {
+  Array.prototype.map = function(callback, thisArg) {
+    'use strict';
+    var T, A, k;
+    if (this === null) throw new TypeError(' this is null or not defined');
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+    if (arguments.length > 1) T = thisArg;
+    A = new Array(len);
+    k = 0;
+    while (k < len) {
+      var kValue, mappedValue;
+      if (k in O) {
+        kValue = O[k];
+        mappedValue = callback.call(T, kValue, k, O);
+        A[k] = mappedValue;
+      }
+      k++;
+    }
+    return A;
+  };
+}
+
+if (!Array.prototype.some) {
+  Array.prototype.some = function(fun/*, thisArg*/) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('Array.prototype.some called on null or undefined');
+    }
+    if (typeof fun !== 'function') throw new TypeError();
+    var t = Object(this);
+    var len = t.length >>> 0;
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t && fun.call(thisArg, t[i], i, t)) return true;
+    }
+    return false;
+  };
+}
+
+if (!Array.prototype.unique) {
+  Array.prototype.unique = function() {
+    'use strict';
+    if (this === void 0 || this === null) throw new TypeError();
+    return this.filter(function(elem, pos, self) {
+      return self.indexOf(elem) === pos;
+    });
+  };
+}
+
+if (!Object.isObject) {
+  Object.isObject = function(arg) {
+    'use strict';
+    return Object.prototype.toString.call(arg) === '[object Object]';
+  };
+}
+
+if (!Object.keys) {
+  Object.keys = function(arg) {
+    'use strict';
+    if (arg !== Object(arg)) {
+      throw new TypeError('Object.keys called on a non-object');
+    }
+    var k=[], p;
+    for (p in arg) {
+      if (Object.prototype.hasOwnProperty.call(arg, p)) {
+        k.push(p);
+      }
+    }
+    return k;
+  };
+}
+
+/**
+ * end Polyfill
+ */
